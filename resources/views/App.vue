86d96a3d298a552a239edf4ff74c0edc06747894
Brandon Park <brandonpark206@gmail.com>
2019-09-22 22:00:08 -0700

    initial vue router setup

---
+++ resources/views/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h1>Riot App</h1>
+
+      <p>
+          <router-link :to="{ name: 'home' }">Home</router-link>
+          <router-link :to="{ name: 'hello' }">Hello</router-link>
+      </p>
+
+      <div class="container">
+          <!--this is where our router will render the given component that matches the route-->
+          <router-view></router-view>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
